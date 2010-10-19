70cb1c9d31b3eae4f04e80eea4f1a41a246c3927
Jed Schneider <jed.schneider@gmail.com>
2010-10-19 15:42:06 -0400

    updating Readme

--- ruby/features/step_definitions/problem2_steps.rb
+++
@@ -1,9 +0,0 @@
-require 'sum_of_even_fibonacci_numbers'
-
-Given /^a fibonacci sequence with a max number of (\d+)$/ do |max_num|
-  @solution = SumOfEvenFibonacciNumbers.new(max_num.to_i)
-end
-
-Then /^the sum of the even numbers should be (\d+)$/ do |sum|
-  @solution.sum.should ==sum.to_i
-end
